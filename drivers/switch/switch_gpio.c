2b91e7e2821af6602ebfd2ab38576b8dc2fa20e9
Romain Caire <romain.caire@doro.com>
2015-10-14 17:06:56 +0200

    Cradle Driver.

--- drivers/switch/switch_gpio.c
+++ drivers/switch/switch_gpio.c
@@ -24,34 +24,44 @@
 #include <linux/switch.h>
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
+#include <linux/of_gpio.h>
 
 struct gpio_switch_data {
 	struct switch_dev sdev;
-	unsigned gpio;
+	int gpio;
 	const char *name_on;
 	const char *name_off;
 	const char *state_on;
 	const char *state_off;
 	int irq;
-	struct work_struct work;
+	struct delayed_work work;
 };
+
+struct gpio_switch_data *data_gpio;
 
 static void gpio_switch_work(struct work_struct *work)
 {
 	int state;
 	struct gpio_switch_data	*data =
-		container_of(work, struct gpio_switch_data, work);
+		container_of(work, struct gpio_switch_data, work.work);
 
 	state = gpio_get_value(data->gpio);
+	pr_info("state:%d,cradle_state:%s.\n",state,(state ? "cradle_off":"cradle_on"));
 	switch_set_state(&data->sdev, state);
 }
+
+int cradle_get_state(void)
+{
+	return gpio_get_value(data_gpio->gpio);
+}
+EXPORT_SYMBOL_GPL(cradle_get_state);
 
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
 {
 	struct gpio_switch_data *switch_data =
 	    (struct gpio_switch_data *)dev_id;
 
-	schedule_work(&switch_data->work);
+	schedule_delayed_work(&switch_data->work,msecs_to_jiffies(100));
 	return IRQ_HANDLED;
 }
 
@@ -61,9 +71,9 @@ static ssize_t switch_gpio_print_state(struct switch_dev *sdev, char *buf)
 		container_of(sdev, struct gpio_switch_data, sdev);
 	const char *state;
 	if (switch_get_state(sdev))
-		state = switch_data->state_on;
-	else
 		state = switch_data->state_off;
+	else
+		state = switch_data->state_on;
 
 	if (state)
 		return sprintf(buf, "%s\n", state);
@@ -72,53 +82,63 @@ static ssize_t switch_gpio_print_state(struct switch_dev *sdev, char *buf)
 
 static int gpio_switch_probe(struct platform_device *pdev)
 {
-	struct gpio_switch_platform_data *pdata = pdev->dev.platform_data;
 	struct gpio_switch_data *switch_data;
 	int ret = 0;
-
-	if (!pdata)
-		return -EBUSY;
-
+	printk("start gpio_switch_probe.\n");
 	switch_data = kzalloc(sizeof(struct gpio_switch_data), GFP_KERNEL);
 	if (!switch_data)
 		return -ENOMEM;
 
-	switch_data->sdev.name = pdata->name;
-	switch_data->gpio = pdata->gpio;
-	switch_data->name_on = pdata->name_on;
-	switch_data->name_off = pdata->name_off;
-	switch_data->state_on = pdata->state_on;
-	switch_data->state_off = pdata->state_off;
+	data_gpio = switch_data;
+	switch_data->sdev.name = "cradle";
+	switch_data->name_on = "on";
+	switch_data->name_off = "off";
+	switch_data->state_on = "1";
+	switch_data->state_off = "0";
 	switch_data->sdev.print_state = switch_gpio_print_state;
-
-	ret = switch_dev_register(&switch_data->sdev);
-	if (ret < 0)
+	printk("pdev->dev.of_node:%s.\n",(pdev->dev.of_node->name));
+	switch_data->gpio = of_get_named_gpio_flags(pdev->dev.of_node,
+				"switch,irq-gpio", 0, NULL);
+	if (gpio_is_valid(switch_data->gpio)) {
+		printk("%s:have get switch gpio_irq:%d.\n",__func__,switch_data->gpio);
+	}else{
+		pr_err("%s:err get switch gpio_irq:%d, need return.\n",__func__,switch_data->gpio);
+		ret = -EBUSY;
 		goto err_switch_dev_register;
-
+	}
+	ret = switch_dev_register(&switch_data->sdev);
+	if (ret < 0){
+		pr_err("err register switch_data.\n");
+		goto err_switch_dev_register;
+	}
 	ret = gpio_request(switch_data->gpio, pdev->name);
-	if (ret < 0)
+	if (ret < 0){
+		pr_err("err request switch gpio %d.\n",switch_data->gpio);
 		goto err_request_gpio;
-
+	}
 	ret = gpio_direction_input(switch_data->gpio);
-	if (ret < 0)
+	if (ret < 0){
+		pr_err("err set switch gpio direection.\n");
 		goto err_set_gpio_input;
-
-	INIT_WORK(&switch_data->work, gpio_switch_work);
+	}
+	INIT_DELAYED_WORK(&switch_data->work, gpio_switch_work);
 
 	switch_data->irq = gpio_to_irq(switch_data->gpio);
 	if (switch_data->irq < 0) {
 		ret = switch_data->irq;
+		pr_err("err get switch gpio irq.\n");
 		goto err_detect_irq_num_failed;
 	}
 
 	ret = request_irq(switch_data->irq, gpio_irq_handler,
-			  IRQF_TRIGGER_LOW, pdev->name, switch_data);
-	if (ret < 0)
+			  IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING | IRQF_ONESHOT, pdev->name, switch_data);
+	if (ret < 0){
+		pr_err("err request switch irq.\n");
 		goto err_request_irq;
-
+	}
 	/* Perform initial detection */
-	gpio_switch_work(&switch_data->work);
-
+	gpio_switch_work(&switch_data->work.work);
+	printk("gpio_switch_probe successfully.\n");
 	return 0;
 
 err_request_irq:
@@ -137,20 +157,24 @@ static int gpio_switch_remove(struct platform_device *pdev)
 {
 	struct gpio_switch_data *switch_data = platform_get_drvdata(pdev);
 
-	cancel_work_sync(&switch_data->work);
+	cancel_delayed_work_sync(&switch_data->work);
 	gpio_free(switch_data->gpio);
 	switch_dev_unregister(&switch_data->sdev);
 	kfree(switch_data);
 
 	return 0;
 }
-
+static const struct of_device_id switch_gpio_table[] = {
+	{.compatible = "switch-gpio"},
+	{}
+};
 static struct platform_driver gpio_switch_driver = {
 	.probe		= gpio_switch_probe,
 	.remove		= gpio_switch_remove,
 	.driver		= {
 		.name	= "switch-gpio",
 		.owner	= THIS_MODULE,
+		.of_match_table = switch_gpio_table,
 	},
 };
 
